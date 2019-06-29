0d03b251cbf9534719dafe919e954acdbef8fc49
Janet Mendez <janetmendez.03@gmail.com>
2019-06-29 07:23:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,8 +4,8 @@ end
 
 def find_max_value(array)
   max_value = 0
-  array.length.times {|index|
-    max_value = (max_value < array[index]) ? array[index]
+  array.length.times { |index|
+    max_value = (max_value < array[index]) ? array[index] : max_value
   }
   max_value
 end
