a965e808fcce221c9fa0790139c3adf0469b3ff9
Janet Mendez <janetmendez.03@gmail.com>
2019-06-29 07:22:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  counter = 0
   max_value = 0
-  while array
+  array.length.times {|index|
+    max_value = (max_value < array[index]) ? array[index]
+  }
+  max_value
 end
 
 def find_min_value(array)
